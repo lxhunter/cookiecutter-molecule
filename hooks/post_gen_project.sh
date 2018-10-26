6bc71bf2fb716c2136116e2f675c1e0d9ead7667
Alexander Jaeger <alexander.jaeger@me.com>
2018-10-26 23:50:44 +0200

    feat: setup hook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-ls
+command -v git >/dev/null 2>&1 || { echo >&2 "I require git but it's not installed.  Aborting."; exit 1; }
+command -v npm >/dev/null 2>&1 || { echo >&2 "I require npm but it's not installed.  Aborting."; exit 1; }
+
+git init
+npm install
